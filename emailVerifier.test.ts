f82f6ca233faeda135f0d31b2a98b803495843e3
seymen-yavas <seymen.yavas@student.vinci.be>
2026-01-16 16:37:06 +0100

    rename variables to English and add comments for clarity

--- emailVerifier.test.ts
+++ emailVerifier.test.ts
@@ -2,11 +2,13 @@ import { emailVerifier } from './emailVerifier';
 
 describe('emailVerifier', () => {
 
+    // a
     test('devrait renvoyer false si l\'email ne contient pas de @', () => {
         const result = emailVerifier('emailpasvalide.com');
         expect(result).toBe(false);
     });
 
+    // b
     test('devrait renvoyer false si le domaine n\'a pas de point', () => {
         expect(emailVerifier('toto@gmailcom')).toBe(false);
     });
@@ -14,17 +16,24 @@ describe('emailVerifier', () => {
     test('devrait renvoyer false si le point est le dernier caractère', () => {
         expect(emailVerifier('toto@gmail.com.')).toBe(false);
     });
-
+    
+    // c
     test('devrait renvoyer false si l\'email contient un espace', () => {
         expect(emailVerifier('user name@domain.com')).toBe(false);
     });
 
+    // d
     test('devrait renvoyer false s\'il n\'y a rien avant le @', () => {
         expect(emailVerifier('@domain.com')).toBe(false);
     });
 
     test('devrait renvoyer false s\'il n\'y a rien après le @', () => {
         expect(emailVerifier('user@')).toBe(false);
+    });
+
+    // if is ok
+    test('devrait renvoyer true pour un email valide', () => {
+        expect(emailVerifier('etudiant.modele@vinci.be')).toBe(true);
     });
 
 });
--- emailVerifier.ts
+++ emailVerifier.ts
@@ -1,19 +1,23 @@
 export function emailVerifier(email: string): boolean {
-    
+    // c
+    if (email.includes(' ')) return false;
+
+    // a
     if (!email.includes('@')) {
         return false;
     }
     
     const splitEmail = email.split('@');
     const domain = splitEmail[1];
-    const utilisateur = splitEmail[0];
+    const username = splitEmail[0];
 
+    // d
+    if (username.length === 0 || domain.length === 0) return false;
+
+    // b
     if (!domain.includes('.')) return false;
-
     if (domain.endsWith('.')) return false;
 
-    if (email.includes(' ')) return false;
-    if (utilisateur.length === 0 || domain.length === 0) return false;
-
+    
     return true;
 }
